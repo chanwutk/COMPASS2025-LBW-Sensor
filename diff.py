2a6866190a3539302e38980fa81d2ddc270481b2
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2025-04-28 23:03:52 +0700

    diff

--- diff.py
+++ diff.py
@@ -6,13 +6,18 @@ ignore = [
     '.github'
 ]
 
+root_files: list[tuple[str, str]] = []
+
 for root, _, files in os.walk('.'):
     for file in files:
         if not any(ignored in root for ignored in ignore):
             if file.endswith('.tex'):
-                current = os.path.join(root, file)
-                original = os.path.join('./_original', root[2:], file)
-                print(os.path.join(root, file))
-                print(os.path.join('./_original', root[2:], file))
+                root_files.append((root, file))
 
-                os.system(f'latexdiff {original} {current} > {current}')
+for root, file in root_files:
+    current = os.path.join(root, file)
+    original = os.path.join('./_original', root[2:], file)
+    print(os.path.join(root, file))
+    print(os.path.join('./_original', root[2:], file))
+
+    os.system(f'latexdiff {original} {current} > {current}')
